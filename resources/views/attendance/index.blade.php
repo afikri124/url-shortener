1875f770ef45e808af129e9b20e0c370f2efed54
Ali Fikri <ali.fikri@kbse.com.my>
2022-10-13 15:55:12 +0700

    capek

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -27,14 +27,14 @@
                         <div class="mb-3">
                             <label for="nama" class="form-label">Name</label>
                             <input type="text" class="form-control @error('name') is-invalid @enderror" id="nama"
-                                name="name" value="{{ Auth::user()->name_with_title }}" disabled  />
+                                name="name" value="{{ Auth::user()->name_with_title }}" disabled />
                         </div>
 
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
                             <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
                                 name="email" @if(Auth::user()->email != null) readonly @endif
-                                value="{{ (old('email') == null ? Auth::user()->email : old('email')) }}" />
+                            value="{{ (old('email') == null ? Auth::user()->email : old('email')) }}" />
                             @error('email')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -43,9 +43,11 @@
                         </div>
 
                         <div class="mb-3">
-                            <label for="user" class="form-label">Username / NIK / NIM / Matrix</label>
-                            <input type="text" class="form-control @error('username') is-invalid @enderror"
-                                id="user" name="username" value="{{ (old('username') == null ? Auth::user()->username : old('username')) }}"  @if(Auth::user()->username != null) readonly @endif />
+                            <label for="user" class="form-label">Username</label>
+                            <input type="text" class="form-control @error('username') is-invalid @enderror" id="user"
+                                name="username" placeholder="NIK / NIM / Matrix ID"
+                                value="{{ (old('username') == null ? Auth::user()->username : old('username')) }}"
+                                @if(Auth::user()->username != null) readonly @endif />
                             @error('username')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -54,31 +56,28 @@
                         </div>
 
                         <div class="mb-3">
-                            <label class="form-label">Activity</label>
-                            <input type="text" class="form-control @error('activity') is-invalid @enderror"
-                                name="activity" value="{{ (old('activity') == null ? $data->title : old('activity')) }}"  @if($data->title != null) readonly @endif/>
-                            @error('activity')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+                            <label class="form-label">Activity (Event/Meeting)</label>
+                            <input type="text" class="form-control"
+                                name="activity" value="{{ $data->title }}" readonly/>
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label">Date</label>
+                            <input type="text" class="form-control" name="date"
+                                value="{{ date('l, d F Y', strtotime($data->date)) }}" readonly />
                         </div>
 
                         <div class="mb-3">
                             <label class="form-label">Location</label>
                             <input type="text" class="form-control @error('location') is-invalid @enderror"
-                                name="location" value="{{ (old('location') == null ? $data->location : old('location')) }}"  @if($data->location != null) readonly @endif/>
-                            @error('location')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+                                name="location"
+                                value="{{ (old('location') == null ? $data->location : old('location')) }}"
+                                @if($data->location != null) readonly @endif/>
                         </div>
                         <hr>
                         <div class="mb-3 text-center">
-                            <button class="btn btn-success w-100" type="submit" name="attendance"
-                            @if($check != null) disabled @endif
-                            ><i class="bx bx-log-in-circle me-2"></i>Attend Now</button>
+                            <button class="btn btn-success w-100" type="submit" name="attendance" @if($check !=null)
+                                disabled @endif><i class="bx bx-log-in-circle me-2"></i>Attend Now</button>
                         </div>
                         <div class="mb-3 text-center">
                             <button class="btn btn-dark w-100" onclick="event.preventDefault();

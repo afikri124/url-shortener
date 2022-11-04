810e73c99e703c57afcddf5c072d36f302480c03
Ali Fikri <ali.fikri@kbse.com.my>
2022-11-04 16:54:56 +0700

    update https

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -217,8 +217,8 @@
 @endsection
 
 @section('script')
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="{{asset('assets/js/jquery.signature.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/geo.js')}}"></script>
 <script type="text/javascript" src="https://www.openlayers.org/api/OpenLayers.js"></script>

c3fb3d0ed957c5722099b6f17cd222684a03a0e8
Ali Fikri <ali.fikri@kbse.com.my>
2023-03-10 22:43:47 +0700

    update

--- app/Http/Controllers/WorkHoursController.php
+++ app/Http/Controllers/WorkHoursController.php
@@ -629,7 +629,7 @@ class WorkHoursController extends Controller
         $data['catatan'] = "<br>Untuk melihat data karyawan secara keseluruhan dapat diakses melalui tautan berikut ini:<br>"
         ."<br><button><b><a target='_blank' href='".url('/WHR')."'>s.jgu.ac.id/WHR</a></b></button>";
         Mail::to($data['email'])->queue(new WeeklyAttendanceReportMail($data));
-        // return new WeeklyAttendanceReportMail($data);
+        Log::info("Weekly report Att sended!");
     }
 
     public function zk(){

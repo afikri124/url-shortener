c4fe05f0de70eab1bb5d7d248cc755dd786c3e78
afikri124 <afikri124@gmail.com>
2023-04-06 10:33:12 +0700

    up

--- resources/views/user/wifi.blade.php
+++ resources/views/user/wifi.blade.php
@@ -16,7 +16,7 @@
 <div class="text-center mt-5 d-flex justify-content-center">
     <div class="mb-3 col-md-4">
         <i class="bx bx-wifi text-success" style="font-size: 50pt"></i>
-        @if(Auth::user()->hasRole('ST') || Auth::user()->hasRole('SD'))
+        @if(Auth::user()->hasRole('ST') || Auth::user()->hasRole('SD') || Auth::user()->hasRole('GS'))
         <h3 class="text-light">Silahkan gunakan akun berikut ini untuk terhubung ke Portal Wifi Jakarta Global University</h3><br>
         <div class="mb-3">
             <label for="user" class="form-label text-light">Username</label>
@@ -27,10 +27,17 @@
             <input type="text" class="form-control  text-center" value="{{$password}}"/>
         </div>
         <br>
+        @if(Auth::user()->hasRole('ST'))
+        @if($password != 'SILAHKAN HUBUNGI ITIC JGU')
         <blockquote class="text-danger"><b>Peringatan!</b><br>Jangan beritahukan <i>username</i> dan 
             <i>password</i> ini kepada siapapun, karena akan mempengaruhi kecepatan internet Anda.</blockquote>
         @else
-        <blockquote class="text-danger mt-3" style="font-size: 15pt"><b>Maaf!</b><br>Anda tidak memiliki akses wifi Kampus, silahkan <i>register</i> sebagai <i>guest</i>.</blockquote>
+        @endif
+        @else
+        <blockquote class="text-danger mt-3" style="font-size: 10pt"><b>Maaf!</b><br>Anda hanya mendapatkan akses terbatas, info lebih lanjut silahkan hubungi ITIC JGU.</blockquote>
+        @endif
+        @else
+        <blockquote class="text-danger mt-3" style="font-size: 15pt"><b>Maaf!</b><br>Anda tidak memiliki akses wifi Kampus, silahkan <i>login</i> sebagai <i>GUEST</i>.</blockquote>
         @endif
     </div>
 </div>

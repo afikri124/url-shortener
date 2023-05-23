64147e4016391c1923bfcdd6e4f9033d78aae5c1
afikri124 <afikri124@gmail.com>
2023-05-23 16:07:04 +0700

    update job schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
         ->withoutOverlapping();
         $schedule->job(new SyncAttendanceJob)->twiceDailyAt(3, 12, 30)->runInBackground()->withoutOverlapping(); //jam 03:30 dan 12:30
         $schedule->job(new SyncAttOnlyJob)->hourly()->runInBackground()->withoutOverlapping();
-        $schedule->job(new BroadCastNotificationDoc)->days([1,2,3,4,5,6])->at('15:30')->runInBackground()->withoutOverlapping();
-        $schedule->job(new WeeklyAttendanceReport)->weeklyOn(5, '17:00')->runInBackground()->withoutOverlapping();
+        $schedule->job(new BroadCastNotificationDoc)->days([1,2,3,4,5,6])->at('16:30')->runInBackground()->withoutOverlapping();
+        $schedule->job(new WeeklyAttendanceReport)->weeklyOn(5, '15:30')->runInBackground()->withoutOverlapping();
     }
 
     /**

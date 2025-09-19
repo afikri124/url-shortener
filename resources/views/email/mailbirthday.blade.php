4ab32c060e5afb9ca8cf96371409659a327075a3
afikri124 <afikri124@gmail.com>
2025-09-19 16:30:14 +0700

    upd

--- resources/views/email/mailbirthday.blade.php
+++ resources/views/email/mailbirthday.blade.php
@@ -19,20 +19,20 @@
                         <h2 style="color:#28a745; text-align:center;">🎂 Selamat Ulang Tahun! 🎂</h2>
                         <h1  style="text-align:center;"><strong>{{ $data['name'] }}</strong></h1>
                         @if (!$data['is_mhs'])
-                        <p>Seluruh keluarga besar <strong>Jakarta Global University</strong> mengucapkan selamat ulang
+                        <p style="text-align:center">Seluruh keluarga besar <strong>Jakarta Global University</strong> mengucapkan selamat ulang
                             tahun untuk Anda.
                             Semoga selalu diberikan kesehatan, kebahagiaan, dan kesuksesan dalam setiap langkah.</p>
-                        <p style="margin-top:20px;">Terima kasih atas dedikasi dan kontribusi Anda dalam membangun
+                        <p style="text-align:center; margin-top:20px;">Terima kasih atas dedikasi dan kontribusi Anda dalam membangun
                             institusi ini. 😊</p>
-                        <p style="text-align:right; margin-top:40px;">Salam hangat,<br><strong>Administrator
+                        <p style="text-align:center; margin-top:40px;">Salam hangat,<br><strong>Administrator
                                 JGU</strong></p>
                         @else
-                        <p>Seluruh civitas akademika <strong>Jakarta Global University</strong> mengucapkan
+                        <p style="text-align:center;">Seluruh civitas akademika <strong>Jakarta Global University</strong> mengucapkan
                             selamat ulang tahun untukmu.
                             Semoga panjang umur, sehat selalu, dan semakin berprestasi dalam perjalanan studi
                             serta kehidupanmu.</p>
-                        <p style="margin-top:20px;">Teruslah bersemangat dalam meraih mimpi dan cita-citamu. 😊</p>
-                        <p style="text-align:right; margin-top:40px;">Salam hangat,<br><strong>Administrator
+                        <p style="text-align:center; margin-top:20px;">Teruslah bersemangat dalam meraih mimpi dan cita-citamu. 😊</p>
+                        <p style="text-align:center; margin-top:40px;">Salam hangat,<br><strong>Administrator
                                 JGU</strong></p>
                         @endif
                     </div>
